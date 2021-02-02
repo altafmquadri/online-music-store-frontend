05cd58e6b1e8b207b2e80fb1c8cfbf3d0849bb5c
Altaf Quadri <altafmquadri@gmail.com>
2021-02-02 14:14:38 -0500

    create songs component

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 
+const SONGSAPI = `http://localhost:8080/api/admin/songs`
+
 class Songs extends Component {
-    state = {  }
+    state = { 
+        songs: []
+     }
+
+    componentDidMount() {
+        fetch(SONGSAPI)
+            .then(res => res.json())
+            .then(data => this.setSongs(data))
+    }
+
+    setSongs = (songs) => {
+        this.setState({ 
+            songs: [...this.state.songs, songs]
+        });
+    }
+
     render() { 
         return ( <h1>Songs</h1> );
     }
