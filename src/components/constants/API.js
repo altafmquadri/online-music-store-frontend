035abbfc18bfd54962ca134b9c67bbc15b9098c9
Altaf Quadri <altafmquadri@gmail.com>
2021-02-05 17:15:42 -0500

    changed api link to correct source

--- src/components/constants/API.js
+++ src/components/constants/API.js
@@ -1,3 +1,3 @@
-export const API_URL = `http://localhost:8080/api`
-// export const API_URL = `https://arcane-mountain-38351.herokuapp.com/api`
+// export const API_URL = `http://localhost:8080/api`
+export const API_URL = `https://arcane-mountain-38351.herokuapp.com/api`
 //test
